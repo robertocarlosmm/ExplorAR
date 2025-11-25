49bcedb1ac44cc2c7590a0cf762aeb699c46d9c6
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2025-11-25 04:27:01 -0500

    pre deploy

--- explorAR/vite.config.js
+++ explorAR/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-    base: '/explorAR/',  
-    plugins: [mkcert()],
-    server: {
-        https: true,
-        host: true,                       // expone en la red local
+export default defineConfig(({ mode }) => {
+    const isProd = mode === 'production'
+
+    return {
+        base: isProd ? '/explorAR/' : '/',   // <- en dev = '/', en build = '/explorAR/'
+        plugins: [mkcert()],
+        server: {
+            https: true,
+            host: true,
+        },
     }
 })
