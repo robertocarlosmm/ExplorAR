7c2fc14d25d1c3a42cdcec7cfea678efb237b4c8
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2025-11-25 04:17:46 -0500

    base

--- explorAR/vite.config.js
+++ explorAR/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+    base: '/explorAR/',  
     plugins: [mkcert()],
     server: {
         https: true,
